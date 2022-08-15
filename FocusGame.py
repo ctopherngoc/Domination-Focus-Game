93ffa3d2dbdd922601792c5a707e7750e441bfe9
Christopher Nguyen <nguyech4@oregonstate.edu>
2022-08-15 16:24:52 -0700

    Revised move_piece function. Split move_piece into smaller helper functions.

--- FocusGame.py
+++ FocusGame.py
@@ -1,4 +1,4 @@
-# The program simulates the focus game as a 6x6 board.
+# The program simulates the domination focus game as a 6x6 board.
 class FocusGame:
     def __init__(self, player1, player2):
         """
@@ -7,15 +7,12 @@ class FocusGame:
 
         self._player1 = player1
         self._player2 = player2
+        self._win = 0
         self._turncounter = 1  # player1 moves on odds, player2 moves on evens
 
         self._dict = {
             self._player1[0]: {"user": self._player1[0], "piece": self._player1[1], "captured": 0, "reserved": 0},
             self._player2[0]: {"user": self._player2[0], "piece": self._player2[1], "captured": 0, "reserved": 0}}
-        counter = 0
-
-        # boolean win condition
-        self._win = 0
 
         self._main_board = [[] for x in range(6)]
 
@@ -27,7 +24,7 @@ class FocusGame:
 
     def printBoard(self):
         """
-        prints board by rows.
+        helper function: prints board by rows.
         """
         for row in self._main_board:
             print(row)
@@ -52,7 +49,7 @@ class FocusGame:
 
     def post_move(self, player, end):
         """
-            Method for scripts post move. Moves pieces into reserve and captured. Verifies if win conditions are met.
+            Method for scripts post move. Move pieces into reserve and captured. Verifies if win conditions are met.
             """
 
         end_point = self._main_board[end[0]][end[1]]
@@ -64,68 +61,103 @@ class FocusGame:
                 self._dict[player]["captured"] += 1
                 end_point[:] = end_point[1:]
 
+    def valid_move_check(self, player, start, end, piece):
+        """
+        Helper function: checks valid movement then if player's piece is on top of stack.
+        """
+        start_point = self._main_board[start[0]][start[1]]
+        end_point = self._main_board[end[0]][end[1]]
+
+        if start == end:
+            return False
+
+        # x axis movement
+        if abs(end[0] - start_point[0]) == piece and end_point[1] == start_point[1]:
+            if start_point[-1] == self._dict[player]["piece"]:
+                return True
+
+        # y axis movement
+        elif abs(end[1] - start_point[1]) == piece and end_point[0] == start_point[0]:
+            if start_point[-1] == self._dict[player]["piece"]:
+                return True
+
+        return False
+
+    def player_turn_check(self, player):
+        if (player == self._player1[0] and self._turncounter % 2 != 0) or (
+                player == self._player2[0] and self._turncounter % 2 == 0):
+            return True
+        else:
+            return False
+
+    def stack_check(self, start, piece):
+        if piece <= len(self._main_board[start[0]][start[1]]):
+            return True
+        else:
+            return False
+
+    def coord_check(self, start, end):
+        """
+        Helper Function: checks coordinates are valid on board.
+        """
+        if start[0] < 0 or start[0] > 5:
+            return False
+        elif start[1] < 0 or start[1] > 5:
+            return False
+        elif end[0] < 0 or end[0] > 5:
+            return False
+        elif end[1] < 0 or end[1] > 5:
+            return False
+        else:
+            return True
+
     def move_piece(self, player, start, end, piece):
         """
-        Method  for single and multiple moves.Containing series of conditionals to determine if move is appropriate.
-        Returns statements if conditionals are not met. If conditionals are met, moves pieces from start to end
-        position then calls post_move method.
+        Method for single and multiple moves. If conditionals are met, moves piece(s).
         """
+
+        # win condition check
+        if self._win == 1:
+            return "Game finished"
+
+        if not self.coord_check(start, end):
+            return "invalid coordinates"
+
         # create variables containing long repetitive references to the board
         start_point = self._main_board[start[0]][start[1]]
         end_point = self._main_board[end[0]][end[1]]
 
-        # win condition check
-        if self._win != 1:
+        if not self.player_turn_check(player):
+            return "not your turn"
 
-            # check if correct player's turn player1 = odd, player2 = even
-            if (player == self._player1[0] and self._turncounter % 2 != 0) or (
-                    player == self._player2[0] and self._turncounter % 2 == 0):
+        if not self.stack_check(start, piece):
+            return "invalid number of pieces"
 
-                # check if amount of piece is <= to pieces at start location and distance move = pieces moved
-                if piece <= len(start_point) and (abs(end[0] - start[0]) == piece or abs(end[1] - start[1]) == piece):
+        if not self.valid_move_check(player, start, end, piece):
+            return "invalid move"
 
-                    # check if move is valid
-                    if start != end:
+        # temp list hold pieces to move. reverse slice list
+        temp_list = start_point[-piece:]
 
-                        # if player's piece is on top of start stack
-                        if start_point[-1] == self._dict[player]["piece"]:
+        # remove pieces from start
+        self._main_board[start[0]][start[1]] = start_point[:len(start_point) - piece]
 
-                            # temp list hold pieces to move
-                            temp_list = self._main_board[start[0]][start[1]][-piece:]
+        # extend end point with pieces
+        end_point.extend(temp_list)
 
-                            # remove pieces from start
-                            self._main_board[start[0]][start[1]] = start_point[:len(start_point) - piece]
+        # increment turn counter
+        self._turncounter += 1
 
-                            # extend end point with pieces
-                            end_point.extend(temp_list)
-
-                            # increment turn counter
-                            self._turncounter += 1
-
-                            # if stack is > 5, calculate reserve and capture checks win condition
-                            if len(end_point) > 5:
-                                self.post_move(player, end)
-                                if self._dict[player]["captured"] >= 6:
-                                    self._win = 1
-                                    return player + " Wins"
-
-                            return "successfully moved"
-
-                        else:
-                            return "invalid piece"
-                    else:
-                        return "invalid location"
-                else:
-                    return "invalid number of pieces"
-            else:
-                return "not your turn"
-        else:
-            return False
+        if len(end_point) > 5:
+            self.post_move(player, end)
+            if self._dict[player]["captured"] >= 6:
+                self._win = 1
+                return player + " Wins"
 
     def reserved_move(self, player, location):
         """
         Method for using pieces in reserve bank to place on board. After conditionals are met and move occurred.
-        Increments turncounter and calls post_move method for victory validation if the.
+        Increments turn counter and calls post_move method for victory validation if the.
         """
         end_point = self._main_board[location[0]][location[1]]
 
